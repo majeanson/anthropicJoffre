e2cef451d48a6c53abe852df14376454910d856d
marc_ <marc_@DESKTOP-30L4RPF>
2025-10-20 22:26:26 -0400

    refactor: Wrap App with context providers
    - Added SocketProvider, GameProvider, SettingsProvider to main.tsx
    - App now has access to all contexts via hooks
    - Build successful - no breaking changes
    - Ready for incremental migration to use contexts
    
    Next: Gradually replace state with context hooks in components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { SocketProvider } from './contexts/SocketContext';
+import { GameProvider } from './contexts/GameContext';
+import { SettingsProvider } from './contexts/SettingsContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SocketProvider>
+      <GameProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </GameProvider>
+    </SocketProvider>
   </React.StrictMode>,
 );
